63c8d88b3002e5a213fcd4ee4ba4af64c1dd3ad5
Rudi Angela <olmeca@me.com>
2017-07-02 12:18:47 +0200

    Added license text

--- lgrep.nim
+++ lgrep.nim
@@ -1,3 +1,28 @@
+#[
+MIT License
+
+Copyright (c) 2017 Rudi Angela
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]#
+
+
 import os
 import parseopt2
 import streams
@@ -5,7 +30,10 @@ import re
 import parseutils
 import "lgrep_lib"
 
-proc writeHelp() = echo """
+const helptext = "lgrep - specialized grep for application log files" & """
+
+  (c) 2017 Rudi Angela
+
   usage: lgrep [options] <regex patterns> <file name>
   
   e.g.   lgrep -i="Exception:" -x="DEBUG" server.log
@@ -96,6 +124,8 @@ proc writeHelp() = echo """
     
     lgrep is based on PCRE (www.pcre.org).
 """
+
+proc writeHelp() = echo helptext
 
 var filename: string = nil
 var printLineNrs: bool = false
